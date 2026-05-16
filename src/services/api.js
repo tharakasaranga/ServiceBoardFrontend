0c25f6dbabb30f4fd08fcc55ebbb8af91c29e65e
Tharaka Saranga <tharakasaranga755@gmail.com>
2026-05-16 18:08:44 +0530

    updated vercel error

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,28 @@
 import axios from "axios";
 
+const rawBaseUrl =
+  process.env.NEXT_PUBLIC_API_URL ||
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  "";
+
+const normalizeBaseUrl = (baseUrl) => {
+  if (!baseUrl) {
+    return "";
+  }
+
+  if (
+    typeof window !== "undefined" &&
+    window.location.protocol === "https:" &&
+    baseUrl.startsWith("http://")
+  ) {
+    return baseUrl.replace(/^http:\/\//, "https://");
+  }
+
+  return baseUrl;
+};
+
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: normalizeBaseUrl(rawBaseUrl),
 });
 
 export default api;
